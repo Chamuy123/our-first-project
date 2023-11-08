f86b71df36945bb37c3e7b7f14dd8b8e0e4a08eb
Chamuy123 <yathamchamundeswari@gmail.com>
2023-11-08 12:35:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponse
+from django.http import HttpResponse
+
+def jampandu(request):
+    return HttpResponse('<h1><marquee>hi  jampandu how rae u</h1></marquee>')
+
+
+
+
+
+def silk(request):
+    return HttpResponse('<h1><marquee>hello silk .it is a very sweet</h1>,</marquee>' )
+
+
+
+
